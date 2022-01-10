0509bc9214f0d09af094bba8bf36d88433d3b01a
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2022-01-09 23:01:38 -0300

    Updates

---
+++ precache-manifest.49b4392c5e692dcaa5081bc0105d11a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d218155c1e3f3a8a4e85985357f55b3",
+    "url": "/appdespesas/index.html"
+  },
+  {
+    "revision": "a351c994cf82caa76810",
+    "url": "/appdespesas/static/css/main.134c36e8.chunk.css"
+  },
+  {
+    "revision": "95f908a1914411187fb6",
+    "url": "/appdespesas/static/js/2.54a45740.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/appdespesas/static/js/2.54a45740.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a351c994cf82caa76810",
+    "url": "/appdespesas/static/js/main.f3f57cc3.chunk.js"
+  },
+  {
+    "revision": "b0ccd4013d0a649d567b",
+    "url": "/appdespesas/static/js/runtime-main.5ff041a8.js"
+  }
+]);
